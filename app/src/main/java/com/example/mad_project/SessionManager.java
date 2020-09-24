7b5d8099e77a53c8afbfa986abbc67f93a53bb75
Gamitha <gamithamanawadu26@gmail.com>
2020-09-24 11:52:06 +0530

    fix errors

--- app/src/main/java/com/example/mad_project/SessionManager.java
+++ app/src/main/java/com/example/mad_project/SessionManager.java
@@ -7,27 +7,31 @@ public class SessionManager {
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
 
-    public SessionManager(Context context){
+    public SessionManager(Context context) {
         sharedPreferences = context.getSharedPreferences("AppKey", 0);
         editor = sharedPreferences.edit();
         editor.apply();
 
     }
-    public  void setLogin(boolean login){
-        editor.putBoolean("KEY_LOGIN",login);
+
+    public void setLogin(boolean login) {
+        editor.putBoolean("KEY_LOGIN", login);
         editor.commit();
     }
 
-    public boolean getLogin(){
+    public boolean getLogin() {
         return sharedPreferences.getBoolean("KEY_LOGIN", false);
     }
 
-    public void setEmail(String Email){
-        editor.putString("KEY_EMAIL",Email);
+    public void setEmail(String Email) {
+        editor.putString("KEY_EMAIL", Email);
         editor.commit();
     }
 
-    public String getEmail(){
+    public String getEmail() {
         return sharedPreferences.getString("KEY_EMAIL", "");
     }
+
 }
+
+
