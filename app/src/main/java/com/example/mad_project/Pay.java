f96b46845cd46e64b7defb9e0e5dd5a2ac4128e9
Gamitha <gamithamanawadu26@gmail.com>
2020-10-12 20:47:51 +0530

    commit

--- app/src/main/java/com/example/mad_project/Pay.java
+++ app/src/main/java/com/example/mad_project/Pay.java
@@ -7,9 +7,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class Pay extends AppCompatActivity {
+
     Context context;
     Button payid;
 
